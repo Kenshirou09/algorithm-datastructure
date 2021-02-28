ae5fbc7f6f2cc41c34c2c32539713dddeea793c7
kenshirou <kenshirou@example.com>
2021-02-28 19:58:05 +0900

    線型探索　添え字取得

---
+++ 全探索/code3.2線型探索添字取得.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define _GLIBCXX_DEBUG
+using ll = long long;
+// using P = pair<int, int>;
+
+int main() {
+  int n,V;
+  cin >> n >> V;
+  vector<int> a(n);
+  rep(i,n) cin >> a[i];
+
+  //線型探索
+  int found_id = -1;
+  for(int i = 0; i < n; i++){
+    if(a[i] == V){
+      found_id = i;
+      break;
+    }
+  }
+  cout << found_id << endl;
+}
