8c93d373b9c01c411dc5f1e58b44887a69979292
kenshirou <kenshirou@example.com>
2021-03-02 17:41:26 +0900

    範囲for文

---
+++ 色々まとめ/範囲for文.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define _GLIBCXX_DEBUG
+using ll = long long;
+// using P = pair<int, int>;
+
+
+//配列のすべての要素に対して何かしらの処理を行いたいときに使う
+int main(){
+    vector<int> a = {1,2,3,4,5};
+    for(int x : a){
+        cout << x << endl;
+    }
+}
